40cf505023e60ca10ad7af7bd2930dea96fd812c
Casper Scheuter <91784787+MassiefStaal@users.noreply.github.com>
2023-09-07 15:42:11 +0200

    wijziging pt2

---
+++ src/main/java/PrimeTester.java
@@ -0,0 +1,36 @@
+import org.apache.commons.math3.primes.Primes;
+
+public class PrimeTester implements Runnable{
+
+    NumberUnderTest numberUnderTest;
+    int highestNumberToTest;
+
+
+    public PrimeTester(NumberUnderTest numberUnderTest, int highestNumberToTest) {
+        this.numberUnderTest = numberUnderTest;
+        this.highestNumberToTest = highestNumberToTest;
+    }
+
+    public void startTesting() {
+
+        while (true) {
+
+            var number = numberUnderTest.getNumber();
+
+            if (number > highestNumberToTest){
+                break;
+            }
+
+            boolean isPrime = Primes.isPrime(number);
+
+            if (isPrime) {
+                System.out.println(Thread.currentThread().getId() + " found a prime number: " + number);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/PrimeTestingApp.java
@@ -0,0 +1,30 @@
+
+
+public class PrimeTestingApp{
+
+        private static final int HIGHEST_NUMBER_TO_TEST = 2000;
+
+        public static void main(String[] args) {
+            var app = new PrimeTestingApp();
+            app.startTesting();
+        }
+
+        private void startTesting() {
+            var numberUnderTest = new NumberUnderTest();
+
+            var tester1 = new PrimeTester(numberUnderTest, HIGHEST_NUMBER_TO_TEST);
+            var tester2 = new PrimeTester(numberUnderTest, HIGHEST_NUMBER_TO_TEST);
+            var tester3 = new PrimeTester(numberUnderTest, HIGHEST_NUMBER_TO_TEST);
+            var tester4 = new PrimeTester(numberUnderTest, HIGHEST_NUMBER_TO_TEST);
+            Thread thread1 = new Thread(tester1);
+            Thread thread2= new Thread(tester2);
+            Thread thread3 = new Thread(tester3);
+            Thread thread4 = new Thread(tester4);
+
+
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+        }
+}
